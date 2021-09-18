cb718a9943e15c716c8d57d6c3d2fa9d48c728e5
Perumal Varadharajulu <perumal98841@gmail.com>
2021-09-18 11:38:59 +0530

    Change LC

--- eat_terraform/env/prod/restapi_lc_asg.tf
+++ eat_terraform/env/prod/restapi_lc_asg.tf
@@ -3,8 +3,8 @@ locals {
 #!/bin/bash
 sudo echo $(aws ecr get-authorization-token --region us-east-2 --output text --query 'authorizationData[].authorizationToken' | base64 -d | cut -d: -f2) | docker login -u AWS 811922229133.dkr.ecr.us-east-2.amazonaws.com --password-stdin
 sudo docker pull 811922229133.dkr.ecr.us-east-2.amazonaws.com/eatzos-prod-api:latest
-#sudo docker run -itd --log-driver=awslogs --log-opt awslogs-region=us-east-2 --log-opt awslogs-group=prod_restapi_log_group_us_east_2 -p 80:8080  811922229133.dkr.ecr.us-east-2.amazonaws.com/eatzos-prod-api:latest
-sudo docker run -itd --log-driver=awslogs --log-opt awslogs-region=us-east-2 --log-opt awslogs-group=prod_restapi_log_group_us_east_2 -p 80:80  811922229133.dkr.ecr.us-east-2.amazonaws.com/eatzos-prod-api:latest
+sudo docker run -itd --log-driver=awslogs --log-opt awslogs-region=us-east-2 --log-opt awslogs-group=prod_restapi_log_group_us_east_2 -p 80:8080  811922229133.dkr.ecr.us-east-2.amazonaws.com/eatzos-prod-api:latest
+#sudo docker run -itd --log-driver=awslogs --log-opt awslogs-region=us-east-2 --log-opt awslogs-group=prod_restapi_log_group_us_east_2 -p 80:80  811922229133.dkr.ecr.us-east-2.amazonaws.com/eatzos-prod-api:latest
 EOF
 }
 
