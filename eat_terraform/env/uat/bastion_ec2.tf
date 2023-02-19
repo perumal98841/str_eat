43cef7c85843e1a419dd839e353e380537e2f691
Perumal Varadharajulu <perumal98841@gmail.com>
2023-02-19 21:03:29 +0530

    Update UAT Spec

--- eat_terraform/env/uat/bastion_ec2.tf
+++ eat_terraform/env/uat/bastion_ec2.tf
@@ -2,10 +2,10 @@ module "uat_bastion" {
     source      = "../../modules/ec2"
     instance_count = 1
     name          = "uat-Windows-Bastion"
-  ami           = "ami-09a54e7baef19d5f4"
+  ami           = "ami-0d8c33aab5be9b992"
   instance_type = "t3a.small"
   subnet_id     = tolist(module.uat_vpc.public_subnets)[0]
-#  private_ips                 = ["10.8.24.10"]
+#  private_ips                 = ["10.8.26.10"]
   vpc_security_group_ids      =   module.uat_bastion_ec2_sg.this_security_group_id
   associate_public_ip_address = true
     business_tags = {
