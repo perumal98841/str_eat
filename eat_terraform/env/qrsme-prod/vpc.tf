a9259cb7edaa479b36129ad6918ee8f49ab9818e
Perumal Varadharajulu <perumal98841@gmail.com>
2024-04-09 23:30:58 +0530

    PROD VPC

---
+++ eat_terraform/env/qrsme-prod/vpc.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_eip" "nat" {
+  count = 0
+   tags = {
+        Owner = "Perumal Varadharajulu"
+        Email = "perumal@strinkit.com"
+        Env = "prod"
+        Product = "QRSME App"
+    } 
+}
+module "prod_vpc" {
+    source      = "../../modules/vpc"
+    aws_region = "us-east-1"
+    name = "prod-vpc"
+    vpcname ="prod-vpc"
+    cidr    = "10.8.30.0/24"
+    enable_dns_hostnames             = true
+    enable_dns_support               = true
+    azs = ["us-east-1a"]
+    public_subnets = ["10.8.30.0/26"]
+    private_subnets = ["10.8.30.128/26"]
+
+    enable_nat_gateway = false
+    single_nat_gateway = false
+    one_nat_gateway_per_az = true
+    #reuse_nat_ips = true
+    #external_nat_ip_ids = "${aws_eip.nat.*.id}"    
+    business_tags = {
+        Owner = "Perumal Varadharajulu"
+        Email = "perumal@strinkit.com"
+    }
+    technical_tags = {
+        Env = "prod"
+        Product = "QRSME App"
+    }
+}
