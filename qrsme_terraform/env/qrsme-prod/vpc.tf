6df513ef84cf73bcd90d848f131916cbed1f8427
Perumal Varadharajulu <perumal98841@gmail.com>
2024-04-25 02:56:28 +0530

    vpc change

--- qrsme_terraform/env/qrsme-prod/vpc.tf
+++ qrsme_terraform/env/qrsme-prod/vpc.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_eip" "nat" {
-  count = 0
+  count = 1
    tags = {
         Owner = "Perumal Varadharajulu"
         Email = "perumal@strinkit.com"
@@ -19,15 +19,15 @@ module "prod_vpc" {
     cidr    = "10.8.30.0/24"
     enable_dns_hostnames             = true
     enable_dns_support               = true
-    azs = ["us-east-1a"]
+    azs = ["us-east-1a","us-east-1b"]
     public_subnets = ["10.8.30.0/26","10.8.30.64/26"]
     private_subnets = ["10.8.30.128/26"."10.8.30.192/26"]
 
     enable_nat_gateway = true
     single_nat_gateway = true
-    one_nat_gateway_per_az = true
-    #reuse_nat_ips = true
-    #external_nat_ip_ids = "${aws_eip.nat.*.id}"    
+    one_nat_gateway_per_az = false
+    reuse_nat_ips = true
+    external_nat_ip_ids = "${aws_eip.nat.*.id}"    
     business_tags = {
         Owner = "Perumal Varadharajulu"
         Email = "perumal@strinkit.com"
