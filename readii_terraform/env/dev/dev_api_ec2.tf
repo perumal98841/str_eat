20feefbad90dcc98f151f7362fe5e54e01f13a4e
Perumal Varadharajulu <perumal98841@gmail.com>
2024-05-29 23:21:38 +0530

    keyname add

--- readii_terraform/env/dev/dev_api_ec2.tf
+++ readii_terraform/env/dev/dev_api_ec2.tf
@@ -4,6 +4,7 @@ module "dev_api" {
     name          = "DEV_EC2"
   ami           = "ami-0cf9618d72642d6dd"
   instance_type = "t2.micro"
+  key_name = "dev-readii-ec2"
   subnet_id     = tolist(module.dev_vpc.public_subnets)[0]
 #  private_ips                 = ["10.8.24.10"]
   vpc_security_group_ids      =   module.dev_api_ec2_sg.this_security_group_id
