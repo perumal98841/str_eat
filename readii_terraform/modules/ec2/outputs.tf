d2a6319da22fe004f04d8d461d2aa5f76ba0d248
Perumal Varadharajulu <perumal98841@gmail.com>
2024-07-06 23:03:37 +0530

    update

--- readii_terraform/modules/ec2/outputs.tf
+++ readii_terraform/modules/ec2/outputs.tf
@@ -1,4 +1,9 @@
-output "this_instancep_id" {
-  description = "The ID of the Instance"
-  value = concat(aws_instance.this.*.id)
+output "id" {
+  description = "The ID of the instance"
+  value = try(
+    aws_instance.this[0].id,
+    aws_instance.ignore_ami[0].id,
+    aws_spot_instance_request.this[0].id,
+    null,
+  )
 }
