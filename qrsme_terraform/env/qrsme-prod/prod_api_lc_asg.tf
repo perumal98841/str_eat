7fbf6f9159394ba7244db54e96fe8ad1692ea0b0
Perumal Varadharajulu <perumal98841@gmail.com>
2024-07-25 08:44:00 +0530

    API update

--- qrsme_terraform/env/qrsme-prod/prod_api_lc_asg.tf
+++ qrsme_terraform/env/qrsme-prod/prod_api_lc_asg.tf
@@ -16,7 +16,7 @@ module "prod_api_lc_asg" {
   # launch_configuration = "my-existing-launch-configuration" # Use the existing launch configuration
   # create_lc = false # disables creation of launch configuration
   lc_name = "prod_api_lc_asg"
-  image_id                     = "ami-04dd99f510c4ff73c"
+  image_id                     = "ami-08f7da7e920b93baf"
   instance_type                = "t3a.small"
   #key_name                     = module.prod_restapi_ec2_keypair.this_key_pair_key_name
   key_name                     = "dev_api"
